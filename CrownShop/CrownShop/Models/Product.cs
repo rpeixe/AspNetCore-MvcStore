e8a16b59df52bf6e7136e8c13fcb67df156a5e96
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-10-18 19:22:51 -0300

    Created Product model

---
+++ CrownShop/CrownShop/Models/Product.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using CrownShop.Enums;
+
+namespace CrownShop.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int PriceInCrowns { get; set; }
+    public ProductType Type { get; set; }
+}
