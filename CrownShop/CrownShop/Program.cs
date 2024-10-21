7d8b15ded15210c1c4aaa7c75aa4d7437c3babc5
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-10-20 23:05:07 -0300

    Configured EF Core and Identity

--- CrownShop/CrownShop/Program.cs
+++ CrownShop/CrownShop/Program.cs
@@ -1,8 +1,20 @@
+using CrownShop.Data;
+using CrownShop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Configure Entity Framework Core
+var connectionString = builder.Configuration.GetConnectionString("SqlServer") ?? throw new InvalidOperationException("Connection string 'SqlServer' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+    
+//Configuration Identity Services
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
