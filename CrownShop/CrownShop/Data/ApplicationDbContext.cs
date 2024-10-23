fe07883a308fd780cb1b4463bf9d3d35cf6c565b
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-10-22 21:35:32 -0300

    Changed primary key type to int for identity entities

--- CrownShop/CrownShop/Data/ApplicationDbContext.cs
+++ CrownShop/CrownShop/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
+using CrownShop.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CrownShop.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>(options)
 {
     
 }
--- CrownShop/CrownShop/Models/ApplicationUser.cs
+++ CrownShop/CrownShop/Models/ApplicationUser.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace CrownShop.Models;
 
-public class ApplicationUser : IdentityUser
+public class ApplicationUser : IdentityUser<int>
 {
     public int CrownBalance { get; set; }
 }
