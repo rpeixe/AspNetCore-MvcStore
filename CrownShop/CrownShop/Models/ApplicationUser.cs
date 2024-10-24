60dbc08aa4be390ad9e4cddad823855cc8014a29
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-10-24 07:57:04 -0300

    Updated user properties

--- CrownShop/CrownShop/Models/ApplicationUser.cs
+++ CrownShop/CrownShop/Models/ApplicationUser.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace CrownShop.Models;
 
 public class ApplicationUser : IdentityUser<int>
 {
-    public int CrownBalance { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime Birthday { get; set; }
 }
