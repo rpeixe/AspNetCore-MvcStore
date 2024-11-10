8209c4007360d88093c0818b9a4bb75c812c018a
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-11-10 08:06:53 -0300

    Added Store Entry entity

---
+++ CrownShop/CrownShop/Models/StoreEntry.cs
@@ -0,0 +1,12 @@
+using CrownShop.Enums;
+
+namespace CrownShop.Models;
+
+public class StoreEntry
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int PriceInCrowns { get; set; }
+    public ItemCategory Category { get; set; }
+    public int MaxQuantity { get; set; }
+}
