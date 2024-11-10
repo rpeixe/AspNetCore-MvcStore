0a82eb14a7a850a214f4613780cf067048ca09c9
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-11-10 07:54:22 -0300

    Added Crown Inventory entity

---
+++ CrownShop/CrownShop/Models/CrownInventory.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrownShop.Models;
+
+public class CrownInventory
+{
+    [Key, ForeignKey("ApplicationUser")]
+    public int Id { get; set; }
+    public required ApplicationUser User { get; set; }
+}
