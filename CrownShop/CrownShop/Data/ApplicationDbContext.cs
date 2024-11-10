f215ddb8e160719f0d7d55bd2c0acba09f6d4139
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-11-10 09:30:33 -0300

    Added entities to DbContext

--- CrownShop/CrownShop/Data/ApplicationDbContext.cs
+++ CrownShop/CrownShop/Data/ApplicationDbContext.cs
@@ -8,5 +8,31 @@ namespace CrownShop.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>(options)
 {
-    
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<CrownInventory> CrownInventories { get; set; }
+    public DbSet<CrownWallet> CrownWallets { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<InventoryItem> InventoryItems { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Membership> Memberships { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<RelatedItem> RelatedItems { get; set; }
+    public DbSet<Sale> Sales { get; set; }
+    public DbSet<StoreEntry> StoreEntrys { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Item>()
+            .HasDiscriminator<string>("ItemType")
+            .HasValue<AccountItem>("AccountItem")
+            .HasValue<InGameItem>("InGameItem");
+        
+        builder.Entity<Purchase>()
+            .HasDiscriminator<string>("PurchaseType")
+            .HasValue<CrownPurchase>("CrownPurchase")
+            .HasValue<ItemPurchase>("ItemPurchase")
+            .HasValue<Subscription>("Subscription");
+    }
 }
