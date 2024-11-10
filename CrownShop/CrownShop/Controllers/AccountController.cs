4860e859ca1dd16b5cf56409233ffe011f357c06
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-11-10 17:31:36 -0300

    Implemented login and logout

--- CrownShop/CrownShop/Controllers/AccountController.cs
+++ CrownShop/CrownShop/Controllers/AccountController.cs
@@ -49,4 +49,45 @@ public class AccountController : Controller
 
         return View(model);
     }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            if (result.RequiresTwoFactor)
+            {
+
+            }
+            if (result.IsLockedOut)
+            {
+
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
 }
